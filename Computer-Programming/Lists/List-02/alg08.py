1ca07ee1762b755d62d43bcf84aec9df65c2b50d
Raissa Cavalcanti <154569377+cavalcantiraissas@users.noreply.github.com>
2024-12-05 09:10:50 -0300

    Create alg08.py

---
+++ Computer-Programming/Lists/List-02/alg08.py
@@ -0,0 +1,19 @@
+########################################
+#Program: alg08.py || list02
+# Author: Raissa Cavalcanti
+# Description: Sequence + Average
+########################################
+
+i = 1
+sum = 0
+
+numbers = int(input("How many numbers will be read?: "))
+
+while (i <= numbers):
+    print(f"Enter the {i} value: ")
+    value = int(input())
+    sum += value
+    i += 1
+
+average = (sum / numbers)
+print(f"Average = {average}")

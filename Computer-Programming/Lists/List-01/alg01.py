1e8c52a72a43911f6d03afc8f2c7db0818362171
Raissa Cavalcanti <154569377+cavalcantiraissas@users.noreply.github.com>
2024-12-05 08:48:24 -0300

    Reading integer numbers

---
+++ Computer-Programming/Lists/List-01/alg01.py
@@ -0,0 +1,10 @@
+########################################
+# Program: alg01.py || list01
+# Author: Raissa Cavalcanti
+# Description: Reading integer numbers
+########################################
+
+first_number = int(input("Enter the first number: \n"))
+second_number = int(input("Enter the second number: \n"))
+
+print(f"You entered the numbers: {first_number} and {second_number}.")

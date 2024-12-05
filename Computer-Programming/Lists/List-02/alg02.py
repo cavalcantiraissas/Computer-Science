0fef7bf586c31552e48a7dd5f9c01410f3b16a3b
Raissa Cavalcanti <154569377+cavalcantiraissas@users.noreply.github.com>
2024-12-05 09:04:05 -0300

    Printing Values (10-1)

---
+++ Computer-Programming/Lists/List-02/alg02.py
@@ -0,0 +1,16 @@
+########################################
+#Program: alg02.py || list02
+# Author: Raissa Cavalcanti
+# Description: Printing Values (10-1)
+########################################
+
+# Counter
+i = 10
+
+# Value
+number = 1
+
+# Counter Implementation
+while (i >= number):
+    print(i)
+    i = i - 1

109920504c37a84933e8c4a10e7bbb5ed48bdbcf
Raissa Cavalcanti <154569377+cavalcantiraissas@users.noreply.github.com>
2024-12-05 09:05:08 -0300

    Printing Values (x-0-x)

---
+++ Computer-Programming/Lists/List-02/alg04.py
@@ -0,0 +1,17 @@
+########################################
+#Program: alg04.py || list02
+# Author: Raissa Cavalcanti
+# Description: Printing Values (x-0-x)
+########################################
+
+number = int(input("Enter a number: "))
+number_2x = number
+i = 0 
+
+while (number >= 0):
+    print(number)
+    number = number - 1
+
+while (i < number_2x):
+    i = i + 1
+    print(i)

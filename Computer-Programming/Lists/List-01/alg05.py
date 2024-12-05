2db0c1f22b74ebdeb40efb846c565f306d57691f
Raissa Cavalcanti <154569377+cavalcantiraissas@users.noreply.github.com>
2024-12-05 08:52:48 -0300

    Number greater than 20

---
+++ Computer-Programming/Lists/List-01/alg05.py
@@ -0,0 +1,12 @@
+########################################
+# Program: alg05.py || list01
+# Author: Raissa Cavalcanti
+# Description: Number greater than 20
+########################################
+
+number = int(input("Enter a number:\n"))
+
+if number > 20:
+    print(f"You chose the number: {number}, and it is greater than 20.")
+else:
+    print("The chosen number is not greater than 20.")

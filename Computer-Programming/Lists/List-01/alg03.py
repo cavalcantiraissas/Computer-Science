b1431a307ee3221da25b5d4cb130d05d93e4bc3d
Raissa Cavalcanti <154569377+cavalcantiraissas@users.noreply.github.com>
2024-12-05 08:50:04 -0300

    Predecessor and Successor

---
+++ Computer-Programming/Lists/List-01/alg03.py
@@ -0,0 +1,12 @@
+########################################
+# Program: alg03.py || list01
+# Author: Raissa Cavalcanti
+# Description: Predecessor and Successor
+########################################
+
+number = int(input("Enter a number:\n"))
+
+predecessor = number - 1
+successor = number + 1
+
+print(f"You chose the number {number}.\nPredecessor: {predecessor}.\nSuccessor: {successor}")

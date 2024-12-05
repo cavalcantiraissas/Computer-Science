1d8e307514f4a06c481daf7dcd51f0991b44302a
Raissa Cavalcanti <154569377+cavalcantiraissas@users.noreply.github.com>
2024-12-05 08:49:26 -0300

    Integer Multiplication

---
+++ Computer-Programming/Lists/List-01/alg02.py
@@ -0,0 +1,11 @@
+########################################
+# Program: alg02.py || list01
+# Author: Raissa Cavalcanti
+# Description: Integer Multiplication
+########################################
+
+first_number = int(input("Enter the first number:\n"))
+second_number = int(input("Enter the second number:\n"))
+
+multiplication = first_number * second_number
+print(f"The multiplication of {first_number} x {second_number} = {multiplication}")

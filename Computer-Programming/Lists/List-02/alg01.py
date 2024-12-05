30050f1fe8812830c8d2cb7186753a9abbcf794e
Raissa Cavalcanti <154569377+cavalcantiraissas@users.noreply.github.com>
2024-12-05 09:03:10 -0300

    Printing Values (0-5)

---
+++ Computer-Programming/Lists/List-02/alg01.py
@@ -0,0 +1,16 @@
+########################################
+#Program: alg01.py || list02
+# Author: Raissa Cavalcanti
+# Description: Printing Values (0-5)
+########################################
+
+# Counter
+i = 0
+
+# Value
+number = 5
+
+# Counter Implementation
+while (i <= number):
+    print(i)
+    i = i + 1

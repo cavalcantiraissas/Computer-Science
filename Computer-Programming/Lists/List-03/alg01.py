61a58856be7a88764ba426b223113cec81f06760
Raissa Cavalcanti <154569377+cavalcantiraissas@users.noreply.github.com>
2024-12-05 09:20:27 -0300

    Vetor (1-5)

---
+++ Computer-Programming/Lists/List-03/alg01.py
@@ -0,0 +1,13 @@
+########################################
+# Programa: alg01.py || lista03
+# Autor: Raissa Cavalcanti
+# Descrição: Vetor (1-5)
+########################################
+
+vetor = []  # Initialize an empty list
+i = 1
+
+while i <= 5:
+    vetor.append(i)  # Add the value of i to the list
+    print(vetor)  # Print the updated list
+    i += 1  # Increment i
